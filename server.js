efc4cb0ea33d6ab222f80406c7cee6cc1d9f0ccc
Laura Brandt <laurambrandt@gmail.com>
2016-10-06 20:42:08 +0000

    added first version of code and added moment dependency

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
+// view on https://fcc-challenges-laurabrandt.c9users.io/
+
 var express = require('express');
+var moment = require('moment');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+function getDates(input) {
+  var date = new Date(input);
+  var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+  var timeString = monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+  return [date.getTime(), timeString];  // unix time, "MMMM DD, YYYY"
+}
+
+app.use(function(request, response) {
+  var timestamp = request.url.slice(1).replace(/%20/g, " ");
+  console.log("string:", timestamp);
+  if (moment(timestamp, "X", true).isValid()) { // unix timestamp
+    var dates = getDates(Number(timestamp));
+  }
+  else if (moment(timestamp, "MMMM D, YYYY", true).isValid()) { // date string 
+    var dates = getDates(timestamp); 
+  }
+  
+  var json = {"unix": null, "natural": null};
+  if (typeof dates !== 'undefined') {
+    json.unix = dates[0];
+    json.natural = dates[1];
+  }
+
+  response.end(JSON.stringify(json));
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('app listening on port 8080');
 });
