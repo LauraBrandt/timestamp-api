ce3de04ae04f80ae931f39517a578001f56f71a9
Laura Brandt <laurambrandt@gmail.com>
2017-05-30 20:05:32 +0000

    redo

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var moment = require('moment');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/:timestamp', function(req, res){
+   res.writeHead(200, { "Content-Type": "text/plain" });
+   var timestamp = req.params.timestamp;
+   var result = {
+     unix: null,
+     natural: null
+   };
+   
+   if (moment(timestamp, 'X').isValid()) {
+     result.unix = moment.unix(timestamp).format('X');
+     result.natural = moment.unix(timestamp).format('MMMM D, YYYY');
+   } else if (moment(timestamp, 'MMMM D, YYYY').isValid()) {
+     result.unix = moment(timestamp).format('X');
+     result.natural = moment(timestamp).format('MMMM D, YYYY');
+   }
+   res.end(JSON.stringify(result));
+});
+
+app.listen(process.env.PORT || 8080);
